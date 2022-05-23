d2871ad81c0688bfabcdff8c130fee5fbf3981fd
unknown <marcusvpaula02@gmail.com>
2022-05-23 11:17:35 -0300

    Create Aula08a.py

---
+++ Aulas/Aula08a.py
@@ -0,0 +1,24 @@
+# from math import sqrt, ceil, floor
+
+# num = int(input("Digite um número: "))
+
+# raiz = sqrt(num)
+
+ #print("A raiz de {} vale {}." .format(num, ceil(raiz)))
+
+# print("Arredondado para baixo vale: {}" .format(floor(raiz)))
+
+# print("A raiz normal vale: {}" .format(raiz))
+
+# import random
+
+# num = random.random()
+
+# num = random.randint(1, 10)
+
+# print(num)
+
+import emoji 
+
+print(emoji.emojize("Olá, violinista :violin:", use_aliases= True))
+
