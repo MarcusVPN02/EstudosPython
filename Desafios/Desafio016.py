9735eca05b935564d9d092aa32672af352afd404
unknown <marcusvpaula02@gmail.com>
2022-05-27 18:11:11 -0300

    Desafio016 feito

--- Desafios/Desafio016.py
+++ Desafios/Desafio016.py
@@ -5,4 +5,4 @@ num = float(input("Digite um número: "))
 
 numint = math.floor(num)
 
-print("O número {} tem a parte inteira {}." .format(num, numint))
+print("O número {} tem a parte inteira {} ou com truncate {}." .format(num, numint, math.trunc(num)))
