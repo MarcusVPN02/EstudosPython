551c4183356b4924ffb7edee1b2b9c13067a61f3
unknown <marcusvpaula02@gmail.com>
2022-05-27 18:24:41 -0300

    Delete b.py

--- Desafios/b.py
+++
@@ -1,7 +0,0 @@
-ca = float(input("Digite o valor do cateto adjacente: "))
-
-co = float(input("Digite o valor do cateto oposto: "))
-
-h = ((ca ** 2) + (co ** 2)) ** (1/2)
-
-print("Os valores são: {} e {}, hipotenusa vale {:.3f}." .format(ca, co, h))
