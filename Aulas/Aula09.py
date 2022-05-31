821747c8801a6aa62db7b80a93ae49f4d86e16d2
unknown <marcusvpaula02@gmail.com>
2022-05-31 10:39:14 -0300

    Fazendo a aula 9 do curso de Python

---
+++ Aulas/Aula09.py
@@ -0,0 +1,44 @@
+frase = ("Curso em vídeo")
+
+''' Fatiamento
+ print(frase[9])
+
+print(frase[9:13])
+
+print(frase[9:13:2])
+
+print(frase[:5])
+
+print(frase[9:])
+
+print(frase[9::3])'''
+
+# Análise
+
+'''print(len(frase))
+
+print(frase.count("o",0,14))
+
+print(frase.find("deo"))
+
+print(frase.find("Android"))
+
+print("Curso" in frase)'''
+
+# Transformação
+
+
+
+'''print(frase.replace("vídeo", "slide"))
+
+print(frase.upper())
+
+print(frase.lower())
+
+print(frase.capitalize())
+
+print(frase.title())'''
+
+# Divisão
+
+# frase.split
---
+++ Aulas/Aula09a.py
@@ -0,0 +1,43 @@
+frase = "   Curso em Vídeo de Python    "
+
+'''print(frase)
+
+print(frase[3])
+
+print(frase[3:13])
+
+print(frase[:13])
+
+print(frase[13:])
+
+print(frase[1:15:2])
+
+print(frase[1::2])
+
+print(frase[::2])'''
+
+# print(frase.count("O"))
+
+# print(frase.upper().count("O"))
+
+'''print(len(frase))
+
+print(len(frase.strip()))
+
+print(frase.replace("Python", "Photoshop").strip())
+
+print(frase[0])
+
+frase = frase.replace("Python" , "Android")
+
+print("Curso" in frase)'''
+
+'''print(frase.find("Curso"))
+
+print(frase.lower().find("vídeo"))'''
+
+dividido = frase.split() # Dividido é objeto
+
+print(dividido[0])
+
+print(dividido [0] [3   ])
