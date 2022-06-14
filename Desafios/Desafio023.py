45ae583daef4fbed41954123cee65812c8a01db6
unknown <marcusvpaula02@gmail.com>
2022-06-14 18:00:05 -0300

    Desafio 23 finalizado

--- Desafios/Desafio023.py
+++ Desafios/Desafio023.py
@@ -1,7 +1,22 @@
-num = (input("Digite um valor entre 0 a 9999: "))
-
-print(num.split())
+num = int(input("Digite um valor entre 0 a 9999: "))
 
 
+# % 10 vai pegaro que está atrás da vírgula
 
+uni = num // 1 % 10
 
+dez = num // 10 % 10
+
+cen = num // 100 % 10
+
+mil = num // 1000 % 10
+
+print("Você digitou {}" .format(num))
+
+print("O valor da unidade é: {}."  .format(uni))
+
+print("O valor da dezena é: {}." .format(dez))
+
+print("O valor da centena é: {}." .format(cen))
+
+print("O valor da unidade de milhar: {}." .format(mil))
