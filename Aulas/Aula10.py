763f07313f5353bff0c50a16a1fd0519af46e1fc
unknown <marcusvpaula02@gmail.com>
2022-07-06 15:01:58 -0300

    Aula 10

---
+++ Aulas/Aula10.py
@@ -0,0 +1,35 @@
+'''c  = int(input("Quanto anos anos o seu carro tem? R: ").strip())
+
+if c < 5:
+    print("Carro novo!!!")
+
+else:
+    print("Carro Velho!!!")
+    
+OU
+    
+
+print("Carro novo!!!" if c < 3 else "Carro velho!!!")
+print("--FIM--")''' 
+
+'''nome = str(input("Nome: "))
+
+if nome == "Marcus":
+    print("O seu nome é muito bonito {}" .format(nome))
+
+else: 
+    print("O seu nome também é bonito!")
+print("Tenha um bom dia, {}!" .format(nome))'''
+
+
+n1 = float(input("Nota 1: "))
+
+n2 = float(input("Nota 2: "))
+
+m = (n1 + n2) / 2
+
+if m < 13:
+    print("Aprovado!!!")
+
+else: 
+    print("Reprovado!!!") 
