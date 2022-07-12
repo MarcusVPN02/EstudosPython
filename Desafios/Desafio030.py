e0cc518035675108595a1fe1fd0f9a35c149f495
unknown <marcusvpaula02@gmail.com>
2022-07-12 09:01:04 -0300

    Create Desafio030.py

---
+++ Desafios/Desafio030.py
@@ -0,0 +1,11 @@
+n = int(input("Digite qualquer número: "))
+
+d = float(n) % 2
+
+if d == 0:
+    print("O número que você digitou foi: {}" .format(n))
+    print("O seu número é par.")
+
+else:
+    print("O número que você digitou foi: {}" .format(n))
+    print("O seu número é impar.") 
