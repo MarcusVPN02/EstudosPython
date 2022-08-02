71225410bdff375991396a08717ce5a44715bbb1
Marcus Nunes <89429373+MarcusVPN02@users.noreply.github.com>
2022-08-02 10:51:18 -0300

    Update Desafio028.py

--- Desafios/Desafio028.py
+++ Desafios/Desafio028.py
@@ -1,15 +1,21 @@
 import random
+from time import sleep
 
-t = int(input("Digite um valor entre 0 a 5 e vejamos se \nvocê pensou no mesmo número que o computador:  ").strip())
+print("-=-" * 20)
 
-x = random.randint(0,5)
+n = int(input("Digite um número entre 0 e 5: ").strip())
 
-if t == x:
-    print("Meus parabéns!!")
-    print("Este é o número que você escolheu: {}" .format(t))
-    print("Este é o número que o computador pensou: {}" .format(x))
+print("-=-" * 20)
 
-else: 
-    print("Que pena, voxê falhou.")
-    print("O seu número: {}" .format(t))
-    print("O número do computador: {}" .format(x))
+a = random.randint(0,5)
+
+print("Processando...")
+
+sleep(2)
+
+if n == a:
+    print("Você ganhou!!!")
+    print("O número pensado era: {}" .format(a))
+else:
+    print("Você falhou!!!")
+    print("O número pensado era: {}" .format(a))
