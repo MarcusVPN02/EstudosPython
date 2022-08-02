1dacfb19a2be5c23bae75b0a5fc3ee5283014a5f
Marcus Nunes <89429373+MarcusVPN02@users.noreply.github.com>
2022-08-02 11:19:17 -0300

    Update Desafio031.py

--- Desafios/Desafio031.py
+++ Desafios/Desafio031.py
@@ -1,13 +1,14 @@
-d = int(input("Qual a distância da viagem em quilômetros? R: ").strip())
+d = int(input("Qual a distância da sua viagem em Km? R: ").strip())
 
-l = d * 0.50
+cm = d * 0.5
+
+cn = d * 0.45
 
 if d <= 200:
-    print("A sua viagem possui uma distância de {}Km." .format(d))
-    print("A sua passagem vai custar R${:.2f}" .format(l))
-    print("Boa viagem!")
-
+    print("A distância da sua viagem: {:.2f}" .format(d))
+    print("O custo da sua viagem será de R${:.2f}" .format(cm))
 else: 
-    print("A sua viagem possui {} de distância" .format(d))
-    print("A sua viagem vai custar R${:.2f}" .format(d * 0.45))
-    print("Boa viagem!")
+    print("A distância da sua viagem: {:.2f}" .format(d))
+    print("O custo da sua viagem será de R${:.2f}" .format(cn))
+    
+    '''OU preço = d * 0.50 if d <= 200 else d * 0.45
