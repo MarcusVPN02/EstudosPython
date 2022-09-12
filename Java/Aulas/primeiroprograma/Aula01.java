c5b9875950a3f246a13a21d93cbcaf97eb30d071
unknown <marcusvpaula02@gmail.com>
2022-09-12 11:15:58 -0300

    Aula01 Java

---
+++ Java/Aulas/primeiroprograma/Aula01.java
@@ -0,0 +1,10 @@
+package primeiroprograma;
+
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+}
+/*Classes e Pacotes estão na IDE NetBeans */
