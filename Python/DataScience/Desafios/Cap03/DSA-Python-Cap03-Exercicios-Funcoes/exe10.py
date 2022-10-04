f54c4eb658f5538f6a0bca9692a984988a9151c3
unknown <marcusvpaula02@gmail.com>
2022-10-04 17:32:29 -0300

    Create exe10.py

---
+++ Python/DataScience/Desafios/Cap03/DSA-Python-Cap03-Exercicios-Funcoes/exe10.py
@@ -0,0 +1,13 @@
+# ************* Desafio ************* (pesquise na documentação Python)
+
+# Exercício 10 - Crie uma função que receba o arquivo abaixo como argumento e retorne um resumo estatístico descritivo 
+# do arquivo. Dica, use Pandas e um de seus métodos, describe()
+# Arquivo: "binary.csv"
+import pandas as pd
+file_name = "binary.csv"
+
+def retornaArq(filename):
+    df = pd.read_csv(filename)
+    return df.describe()
+
+retornaArq(file_name)
