f418e995779cd75e377a53d942f12d6e7e5c3b85
unknown <marcusvpaula02@gmail.com>
2022-10-17 19:38:40 -0300

    Ajuste

--- Python/DataScience/Desafios/Cap03/DSA-Python-Cap03-Exercicios-Funcoes/exe09.py
+++ Python/DataScience/Desafios/Cap03/DSA-Python-Cap03-Exercicios-Funcoes/exe09.py
@@ -2,5 +2,5 @@
 # Abaixo você encontra a importação do Pandas, um dos principais pacotes Python para análise de dados.
 # Analise atentamente todos os métodos disponíveis. Um deles você vai usar no próximo exercício.
 
-import pandas as pd
-dir(pd)
+#import pandas as pd
+#dir(pd)
--- Python/DataScience/Desafios/Cap03/DSA-Python-Cap03-Exercicios-Funcoes/exe10.py
+++ Python/DataScience/Desafios/Cap03/DSA-Python-Cap03-Exercicios-Funcoes/exe10.py
@@ -3,11 +3,11 @@
 # Exercício 10 - Crie uma função que receba o arquivo abaixo como argumento e retorne um resumo estatístico descritivo 
 # do arquivo. Dica, use Pandas e um de seus métodos, describe()
 # Arquivo: "binary.csv"
-import pandas as pd
+'''import pandas as pd
 file_name = "binary.csv"
 
-def retornaArq(filename):
+#def retornaArq(filename):
     df = pd.read_csv(filename)
     return df.describe()
 
-retornaArq(file_name)
+retornaArq(file_name)'''
